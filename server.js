19584376c277e0918adff953ff8d2c389ea7083d
shshinde2580-eng <shshinde2580@gmail.com>
2025-11-14 21:03:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,29 +7,26 @@ const PORT = process.env.PORT || 3000;
 // Serve public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-// Default route – Home page
+// Default route - Home page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-// NOTES route
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "Notes", "notes.html"));
-});
-
-// SYLLABUS route
+// Syllabus page
 app.get("/syllabus", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "Syllabus", "Syllabus.html"));
 });
 
-// PAPERS route
+// Notes page
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "Notes", "Notes.html"));
+});
+
+// Papers page
 app.get("/papers", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "papers", "index.html"));
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-
